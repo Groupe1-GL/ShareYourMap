011677843264ad2a8adc3322609f7ce2d138716c
PC0 <PC0@PC-01>
2020-03-18 12:22:34 +0100

    DAO interface update

--- ShareYourMapV2/src/main/java/com/example/jetty_jersey/ws/MapDAO.java
+++ ShareYourMapV2/src/main/java/com/example/jetty_jersey/ws/MapDAO.java
@@ -2,12 +2,17 @@ package com.example.jetty_jersey.ws;
 
 import java.util.List;
 
+/**
+ * MapDAO is the interface of the Data Access Object of maps.
+ *
+ * @author Eric Gao
+ * @version 1.0
+ * @since 1.0
+ */
 public interface MapDAO {
-	public Map getPublicMaps(boolean access); // return all public maps
-	
-	/*
-	//getLocations...
-	public List<Location> getLocations();
-	public boolean addLocation(Location l);
-	*/
+	public List<Map> getMaps();
+	public Map getMap(int mid);
+	public boolean editMap(int mid, String name);
+	public boolean deleteMap(int mid);
+	public List<Location> getLocations(int mid);
 }
--- ShareYourMapV2/src/main/java/com/example/jetty_jersey/ws/UserDAO.java
+++ ShareYourMapV2/src/main/java/com/example/jetty_jersey/ws/UserDAO.java
@@ -1,14 +1,48 @@
 package com.example.jetty_jersey.ws;
 import java.util.List;
 
+import javax.ws.rs.core.Response;
+
+/**
+ * UserDAO is the interface of the Data Access Object of users.
+ *
+ * @author Eric Gao
+ * @version 1.0
+ * @since 1.0
+ */
 public interface UserDAO {
-	public User getUsers(); // return all users in the database
-	/*
-	public int getUserID();
-	public String getUserInfo();
-	public List<Map> getMaps();  //return map's name
-	public void putMap(Map m);  //Add a favorite to the map’s list
-	public void deleteMap(Map m);  //Delete a favorite to the map’s list
-	public Position getCurrent_Position();  // return current position
-	*/
+	public User getUsers();
+	public Response createUser( 		String name, 
+										String password, 
+										String cpassword, 
+										String email);
+	public User getUser(				int uid);
+	public Response editUser(   		int uid, 
+										String opassword, 
+										String password, 
+										String cpassword, 
+										String email);
+	public boolean deleteUser(			int uid);
+	public List<Map> getMapsOfUser(		int uid); 
+	public boolean createMap(			int uid, String name);
+	public boolean addMapOnUser(		int uid, int mid);
+	public boolean removeMapOnUser(		int uid, int mid);
+	public boolean addLocationOnMap(	int uid, 
+										int mid, 
+										String name, 
+										String descr, 
+										String label, 
+										float x, 
+										float y);
+	public boolean editLocation(		int uid,
+										int mid,
+										int lid,
+										String message);
+	public boolean contributeOnLocation(int uid,
+										int mid,
+										int lid,
+										String message);
+	public boolean deleteLocation(		int uid,
+										int mid,
+										int lid);
 }
